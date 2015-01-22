ce20da5d199f45462d245e8d438e95374a6e7150
Dirk Kelly & Nathan Harper <iex-eng+dirkkelly+nathanharper@googlegroups.com>
2015-01-22 10:59:34 -0500

    ClientLinkRequest interface

--- lib/CampMinder/ClientLinkRequest.rb
+++ lib/CampMinder/ClientLinkRequest.rb
@@ -2,9 +2,19 @@ class CampMinder::ClientLinkRequest
   include Virtus.model
 
   attribute :username, String
+  attribute :password, String
+  attribute :signed_object, String
+  attribute :token, String
+  attribute :client_id, String
+  attribute :person_id, String
 
   def initialize(args)
     @username = args.fetch('username')
+    @password = args.fetch('password')
+    @signed_object = args.fetch('signedObject')
+    @token = args.fetch('token')
+    @client_id = args.fetch('clientID')
+    @person_id = args.fetch('personID')
   end
 
   def valid?
--- spec/CampMinder/ClientLinkRequest_spec.rb
+++ spec/CampMinder/ClientLinkRequest_spec.rb
@@ -23,11 +23,25 @@ describe CampMinder::ClientLinkRequest do
       expect(@client_link_request.username).to eq 'johndoe'
     end
 
-    it 'assigns the password attribute'
-    it 'assigns the signed_object attribute'
-    it 'assigns the token attribute'
-    it 'assigns the client_id attribute'
-    it 'assigns the person_id attribute'
+    it 'assigns the password attribute' do
+      expect(@client_link_request.password).to eq 'secret'
+    end
+
+    it 'assigns the signed_object attribute' do
+      expect(@client_link_request.signed_object).to eq 'ABC.123'
+    end
+
+    it 'assigns the token attribute' do
+      expect(@client_link_request.token).to eq 'DEF-456'
+    end
+
+    it 'assigns the client_id attribute' do
+      expect(@client_link_request.client_id).to eq 'C-123'
+    end
+
+    it 'assigns the person_id attribute' do
+      expect(@client_link_request.person_id).to eq 'P-123'
+    end
   end
 
   describe '#valid?' do
