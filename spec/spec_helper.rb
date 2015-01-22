a830b119660d76c36ac98152970e65aa7c2f9a36
Dirk Kelly <iex-eng+dirkkelly@googlegroups.com>
2015-01-22 07:41:02 -0500

    codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'dotenv'
 puts Dotenv.load
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'CampMinder'
