346d7ff5c358f5dd62edd835bda4c7d0dc10293d
Nathan Harper <iex-eng+nathanharper@googlegroups.com>
2015-01-26 17:00:30 -0500

    specs for unimplemented handler methods

--- lib/CampMinder/controllers/handler_controller.rb
+++ lib/CampMinder/controllers/handler_controller.rb
@@ -16,12 +16,23 @@ module CampMinder::HandlerController
     success = @client_link_request.valid_expiration_time?
     reason = @client_link_request.invalid_reason
 
+    partner_client_id = nil
+
+    if success
+      partner_client_id = verify_username_password(@client_link_request.username, @client_link_request.password)
+
+      if !partner_client_id
+        success = false
+        reason = 'invalid username and password'
+      end
+    end
+
     if success
       connection = CampMinder::EstablishConnection.new(
         'clientID' => @client_link_request.client_id,
         'personID' => @client_link_request.person_id,
         'token' => @client_link_request.token,
-        'partnerClientID' => "TODO"
+        'partnerClientID' => partner_client_id
       )
 
       if !connection.connect
@@ -30,10 +41,29 @@ module CampMinder::HandlerController
       end
     end
 
+    if success
+      success = store_partner_client(
+        partner_client_id,
+        @client_link_request.client_id,
+        @client_link_request.person_id,
+        @client_link_request.token,
+        1 # TODO: what is connection status for? If we've reached this point, they are presumably connected.
+      )
+      reason = 'failed to save partner client' unless success
+    end
+
     redirect_to "#{CampMinder::REDIRECTION_URL}?bpid=#{CampMinder::BUSINESS_PARTNER_ID}&success=#{success}&reason=#{reason}", status: 304
   end
 
   def server_time_get
     render xml: CampMinder::ServerTimeGet.new, root: 'responseObject'
+  end
+
+  def verify_username_password(username, password)
+    raise NotImplementedError
+  end
+
+  def store_partner_client(partner_client_id, client_id, person_id, token, connection_status)
+    raise NotImplementedError
   end
 end
--- spec/dummy/spec/CampMinderHandler_spec.rb
+++ spec/dummy/spec/CampMinderHandler_spec.rb
@@ -9,16 +9,35 @@ describe 'CampMinderHandler' do
 
   describe 'ClientLinkRequest' do
     before do
+      Timecop.freeze(Time.now)
+
       @signed_request_factory = CampMinder::SignedRequestFactory.new(CampMinder::SECRET_CODE)
+
+      success_base64_time = Base64.encode64((Time.now.utc + 10).iso8601.to_s)
+      @success_signed_object = "#{@signed_request_factory.encode_signature(success_base64_time)}.#{success_base64_time}"
+
+      failure_base64_time = Base64.encode64((Time.now.utc - 10).iso8601.to_s)
+      @failure_signed_object = "#{@signed_request_factory.encode_signature(failure_base64_time)}.#{failure_base64_time}"
+
+      @username = 'johndoe'
+      @password = 'secret'
+      @partner_client_id = 'PC-100'
+      @client_id = 'C-123'
+      @person_id = 'P-123'
+      @token = 'DEF-456'
+      @connection_status = 1
+    end
+
+    after do
+      Timecop.return
     end
 
     it 'redirects with success on all good' do
-      time = (Time.now.utc + 10).iso8601.to_s
-      base64_time = Base64.encode64(time)
-      signed_object = @signed_request_factory.encode_signature(base64_time) + '.' + base64_time
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:verify_username_password).with(@username, @password).and_return(@partner_client_id)
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:store_partner_client).with(@partner_client_id, @client_id, @person_id, @token, @connection_status).and_return(true)
 
       VCR.use_cassette("ClientLinkRequestSuccess") do
-        post '/camp_minder_handler', fn: 'ClientLinkRequest', username: 'johndoe', password: 'secret', signedObject: signed_object, token: 'DEF-456', clientID: 'C-123', personID: 'P-123'
+        post '/camp_minder_handler', fn: 'ClientLinkRequest', username: @username, password: @password, signedObject: @success_signed_object, token: @token, clientID: @client_id, personID: @person_id
       end
 
       expect(last_response.status).to eq 304
@@ -26,27 +45,63 @@ describe 'CampMinderHandler' do
     end
 
     it 'redirects with failure on invalid time' do
-      time = (Time.now.utc - 10).iso8601.to_s
-      base64_time = Base64.encode64(time)
-      signed_object = @signed_request_factory.encode_signature(base64_time) + '.' + base64_time
-
-      post '/camp_minder_handler', fn: 'ClientLinkRequest', username: 'johndoe', password: 'secret', signedObject: signed_object, token: 'DEF-456', clientID: 'C-123', personID: 'P-123'
+      post '/camp_minder_handler', fn: 'ClientLinkRequest', username: @username, password: @password, signedObject: @failure_signed_object, token: @token, clientID: @client_id, personID: @person_id
 
       expect(last_response.status).to eq 304
       expect(last_response.location).to eq "#{CampMinder::REDIRECTION_URL}?bpid=#{CampMinder::BUSINESS_PARTNER_ID}&success=false&reason=Invalid GetLinkRequest - signature was invalid!"
     end
 
     it 'redirects with failure on EstablishConnection failure' do
-      time = (Time.now.utc + 10).iso8601.to_s
-      base64_time = Base64.encode64(time)
-      signed_object = "#{@signed_request_factory.encode_signature(base64_time)}.#{base64_time}"
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:verify_username_password).and_return(@partner_client_id)
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:store_partner_client).and_return(true)
 
       VCR.use_cassette("ClientLinkRequestEstablishConnectionFailure") do
-        post '/camp_minder_handler', fn: 'ClientLinkRequest', username: 'johndoe', password: 'secret', signedObject: signed_object, token: 'DEF-456', clientID: 'C-123', personID: 'P-123'
+        post '/camp_minder_handler', fn: 'ClientLinkRequest', username: @username, password: @password, signedObject: @success_signed_object, token: @token, clientID: @client_id, personID: @person_id
       end
 
       expect(last_response.status).to eq 304
       expect(last_response.location).to eq "#{CampMinder::REDIRECTION_URL}?bpid=#{CampMinder::BUSINESS_PARTNER_ID}&success=false&reason=Unknown"
+    end
+
+    it 'throws an error when verify_username_password is unimplemented' do
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:store_partner_client).and_return(true)
+
+      expect do
+        VCR.use_cassette("ClientLinkRequestSuccess") do
+          post '/camp_minder_handler', fn: 'ClientLinkRequest', username: @username, password: @password, signedObject: @success_signed_object, token: @token, clientID: @client_id, personID: @person_id
+        end
+      end.to raise_error NotImplementedError
+    end
+
+    it 'redirects with failure on verify_username_password failure' do
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:verify_username_password).and_return(nil)
+
+      post '/camp_minder_handler', fn: 'ClientLinkRequest', username: @username, password: @password, signedObject: @success_signed_object, token: @token, clientID: @client_id, personID: @person_id
+
+      expect(last_response.status).to eq 304
+      expect(last_response.location).to eq "#{CampMinder::REDIRECTION_URL}?bpid=#{CampMinder::BUSINESS_PARTNER_ID}&success=false&reason=invalid username and password"
+    end
+
+    it 'throws an error when store_partner_client is unimplemented' do
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:verify_username_password).and_return(@partner_client_id)
+
+      expect do
+        VCR.use_cassette("ClientLinkRequestSuccess") do
+          post '/camp_minder_handler', fn: 'ClientLinkRequest', username: @username, password: @password, signedObject: @success_signed_object, token: @token, clientID: @client_id, personID: @person_id
+        end
+      end.to raise_error NotImplementedError
+    end
+
+    it 'redirects with failure on store_partner_client failure' do
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:verify_username_password).and_return(@partner_client_id)
+      allow_any_instance_of(CampMinder::HandlerController).to receive(:store_partner_client).and_return(false)
+
+      VCR.use_cassette("ClientLinkRequestSuccess") do
+        post '/camp_minder_handler', fn: 'ClientLinkRequest', username: @username, password: @password, signedObject: @success_signed_object, token: @token, clientID: @client_id, personID: @person_id
+      end
+
+      expect(last_response.status).to eq 304
+      expect(last_response.location).to eq "#{CampMinder::REDIRECTION_URL}?bpid=#{CampMinder::BUSINESS_PARTNER_ID}&success=false&reason=failed to save partner client"
     end
   end
 
