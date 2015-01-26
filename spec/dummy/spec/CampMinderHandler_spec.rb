64830fbbeddb12e8baa789d8dbb8678388530970
Dirk Kelly & Nathan Harper <iex-eng+dirkkelly+nathanharper@googlegroups.com>
2015-01-26 14:30:48 -0500

    failing spec waiting on incorrect return

--- spec/dummy/spec/CampMinderHandler_spec.rb
+++ spec/dummy/spec/CampMinderHandler_spec.rb
@@ -10,9 +10,14 @@ describe 'CampMinderHandler' do
   describe 'ClientLinkRequest' do
     before do
       @signed_request_factory = CampMinder::SignedRequestFactory.new(CampMinder::SECRET_CODE)
+      stub_request(:post, CampMinder::WEB_SERVICE_URL).
+        to_return(body: %(<?xml version="1.0" encoding="UTF-8"?>
+          <responseObject version="1">
+            <Success>True</Success>
+          </responseObject>))
     end
 
-    it 'redirects on success' do
+    it 'redirects with success on all good' do
       time = (Time.now.utc + 10).iso8601.to_s
       base64_time = Base64.encode64(time)
       signed_object = @signed_request_factory.encode_signature(base64_time) + '.' + base64_time
@@ -22,7 +27,7 @@ describe 'CampMinderHandler' do
       expect(last_response.location).to eq "#{CampMinder::REDIRECTION_URL}?bpid=#{CampMinder::BUSINESS_PARTNER_ID}&success=true&reason="
     end
 
-    it 'redirects on failure' do
+    it 'redirects with failure on invalid time' do
       time = (Time.now.utc - 10).iso8601.to_s
       base64_time = Base64.encode64(time)
       signed_object = @signed_request_factory.encode_signature(base64_time) + '.' + base64_time
@@ -30,6 +35,16 @@ describe 'CampMinderHandler' do
 
       expect(last_response.status).to eq 304
       expect(last_response.location).to eq "#{CampMinder::REDIRECTION_URL}?bpid=#{CampMinder::BUSINESS_PARTNER_ID}&success=false&reason=Invalid GetLinkRequest - signature was invalid!"
+    end
+
+    it 'redirects on connection failure' do
+      time = (Time.now.utc + 10).iso8601.to_s
+      base64_time = Base64.encode64(time)
+      signed_object = @signed_request_factory.encode_signature(base64_time) + '.' + base64_time
+      post '/camp_minder_handler', fn: 'ClientLinkRequest', username: 'johndoe', password: 'secret', signedObject: signed_object, token: 'DEF-456', clientID: 'C-123', personID: 'P-123'
+
+      expect(last_response.status).to eq 304
+      expect(last_response.location).to eq "#{CampMinder::REDIRECTION_URL}?bpid=#{CampMinder::BUSINESS_PARTNER_ID}&success=false&reason=unknown"
     end
   end
 
